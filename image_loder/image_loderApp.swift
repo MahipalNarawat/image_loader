1f0a80b948f851b702abede861f66dc8187aa5fb
Mahipal Singh Rathore <mahipalsnarawat@gmail.com>
2024-04-16 21:41:24 +0530

    Initial Commit

---
+++ image_loder/image_loderApp.swift
@@ -0,0 +1,17 @@
+//
+//  image_loderApp.swift
+//  image_loder
+//
+//  Created by Mahipal on 16/04/24.
+//
+
+import SwiftUI
+
+@main
+struct image_loderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
